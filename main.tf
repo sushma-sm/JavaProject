252b50098c77dbe2256faca02dbeaaf594bd38b1
sushma-sm <gurajasushmamounika@gmail.com>
2025-03-04 10:26:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,17 +4,6 @@ provider "google" {
   zone    = "us-central1-a"  # Specify the zone for VM
 }
 
-# Generate a random ID to append to the bucket name
-resource "random_id" "bucket_suffix" {
-  byte_length = 7
-}
-
-# Create a Cloud Storage Bucket
-resource "google_storage_bucket" "my_bucket" {
-  name     = "my-terraform-bucket-${random_id.bucket_suffix.hex}"
-  location = "US"
-}
-
 # Create a VM instance
 resource "google_compute_instance" "my_vm" {
   name         = "my-terraform-vm"
