2f0cb5a02673d48c78fc834ec8dce4a759ff2f75
sushma-sm <gurajasushmamounika@gmail.com>
2025-03-13 17:13:53 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,4 +1,20 @@
-provider "google" {
-  project     = var.project_id
-  region      = var.region
+terraform {
+  required_version = ">= 1.5.7"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.80.0"  # You can adjust this to the latest version
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.5.1"  # Adjust this to the latest version as needed
+    }
+  }
 }
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+provider "random" {}
