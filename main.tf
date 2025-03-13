355bbb6162a133d132a8ef4cbe443f4a37469570
sushma-sm <gurajasushmamounika@gmail.com>
2025-03-13 16:42:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,52 +1,15 @@
-terraform {
-  required_providers {
-    helm = {
-      source = "hashicorp/helm"
-      version = "2.7.1"
-    }
-    kubernetes = {
-      source = "hashicorp/kubernetes"
-      version = "2.11.0"
-    }
+resource "google_container_cluster" "monitoring_cluster" {
+  name     = var.monitoring_cluster_name
+  location = var.monitoring_region
+
+  deletion_protection = false
+
+  initial_node_count = var.monitoring_node_count
+
+  node_config {
+    machine_type = var.monitoring_machine_type
+    disk_size_gb = 70
   }
-}
 
-provider "helm" {
-  kubernetes {
-    config_path = "~/.kube/config"
-  }
-}
-
-provider "kubernetes" {
-  config_path = "~/.kube/config"
-}
-
-resource "kubernetes_namespace" "monitoring" {
-  metadata {
-    name = "monitoring"
-  }
-}
-
-resource "helm_release" "prometheus" {
-  name       = "prometheus"
-  namespace  = kubernetes_namespace.monitoring.metadata[0].name
-  chart      = "prometheus-community/kube-prometheus-stack"
-  repository = "https://prometheus-community.github.io/helm-charts"
-  version    = "51.2.0"
-
-  values = [
-    file("prometheus-values.yaml")
-  ]
-}
-
-resource "helm_release" "grafana" {
-  name       = "grafana"
-  namespace  = kubernetes_namespace.monitoring.metadata[0].name
-  chart      = "grafana/grafana"
-  repository = "https://grafana.github.io/helm-charts"
-  version    = "6.56.2"
-
-  values = [
-    file("grafana-values.yaml")
-  ]
+  remove_default_node_pool = false
 }
