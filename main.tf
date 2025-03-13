5b10362a9323900fe2cbcfddb7e375d23a9c157d
sushma-sm <gurajasushmamounika@gmail.com>
2025-03-13 13:33:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,28 +1,52 @@
-provider "google" {
-  project = "devops-practice-sm"
-  region  = "us-central1"
-  zone    = "us-central1-a"
-}
-
-# Generate a random ID for uniqueness
-resource "random_id" "vm_suffix" {
-  byte_length = 4
-}
-
-# Create a VM instance with a unique name
-resource "google_compute_instance" "my_vm" {
-  name         = "my-terraform-vm-${random_id.vm_suffix.hex}"
-  machine_type = "e2-medium"
-  zone         = "us-central1-a"
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
+terraform {
+  required_providers {
+    helm = {
+      source = "hashicorp/helm"
+      version = "2.7.1"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.11.0"
     }
   }
+}
 
-  network_interface {
-    network = "default"
-    access_config {}
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+resource "helm_release" "prometheus" {
+  name       = "prometheus"
+  namespace  = "monitoring"
+  chart      = "prometheus-community/kube-prometheus-stack"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  version    = "51.2.0"
+
+  values = [
+    file("prometheus-values.yaml")
+  ]
+}
+
+resource "helm_release" "grafana" {
+  name       = "grafana"
+  namespace  = "monitoring"
+  chart      = "grafana/grafana"
+  repository = "https://grafana.github.io/helm-charts"
+  version    = "6.56.2"
+
+  values = [
+    file("grafana-values.yaml")
+  ]
+}
+
+resource "kubernetes_namespace" "monitoring" {
+  metadata {
+    name = "monitoring"
   }
 }
