ccca9b26777606baf2854a99e160d3da46fbb0a1
sushma-sm <gurajasushmamounika@gmail.com>
2025-03-14 18:25:43 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -20,7 +20,6 @@ provider "google" {
   project     = var.project_id
   region      = var.region
   zone        = var.zone
-  credentials = var.gcp_credentials  # Reference to credentials variable
 }
 
 provider "random" {}
