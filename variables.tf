0c4611f87099e9710993746ac5f779a6e03e2657
sushma-sm <gurajasushmamounika@gmail.com>
2025-03-14 18:10:38 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,26 +4,22 @@ variable "project_id" {
   default     = "devops-practice-sm"
 }
 
+# Changed from "us-east5-b" (zone) to "us-east5" (region)
 variable "region" {
-  description = "The region where the resources will be created"
+  description = "The GCP region (not zone)"
   type        = string
-  default     = "us-east5-b"  # Updated to match the existing cluster location
+  default     = "us-east5"  # Region without zone suffix
+}
+
+# New variable for zone specification
+variable "zone" {
+  description = "The GCP zone"
+  type        = string
+  default     = "us-east5-b"  # Specific zone
 }
 
 variable "cluster_name" {
-  description = "The name of the Kubernetes cluster"
+  description = "The name of the existing Kubernetes cluster"
   type        = string
-  default     = "cluster-6"  # Updated to match the existing cluster name
-}
-
-variable "node_count" {
-  description = "The number of nodes in the Kubernetes cluster"
-  type        = number
-  default     = 3  # Keeping this as a reference, even though the cluster already exists
-}
-
-variable "node_machine_type" {
-  description = "The type of machine to use for nodes in the Kubernetes cluster"
-  type        = string
-  default     = "e2-medium"  # Keeping this as a reference for future cluster creations
+  default     = "cluster-6"
 }
