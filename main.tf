7e13744a5fa88363429d83273122f4538bd0cb7a
sushma-sm <gurajasushmamounika@gmail.com>
2025-03-14 13:03:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,12 @@ resource "google_container_cluster" "monitoring_cluster" {
   node_config {
     machine_type = var.node_machine_type
     disk_size_gb = 70
+    oauth_scopes = ["https://www.googleapis.com/auth/cloud-platform"]
   }
 
   remove_default_node_pool = false
+
+  # Enable logging and monitoring for better observability
+  logging_service    = "logging.googleapis.com/kubernetes"
+  monitoring_service = "monitoring.googleapis.com/kubernetes"
 }
